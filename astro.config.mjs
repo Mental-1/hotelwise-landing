810b60bc6a47e2b0568aca622a13b466de393bdf
Mental-1 <njengalewis133@gmail.com>
2026-07-01 11:37:18 +0300

    add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mental-1.github.io",
+  base: "/hotelwise-landing",
   integrations: [react()]
 });
